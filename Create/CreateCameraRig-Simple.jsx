7874342657b215d18e5dc9e888dffc8f97f1a7ac
h3llolovely <101287022+h3llolovely@users.noreply.github.com>
2026-03-14 12:33:41 -0600

    Update CreateCameraRig-Simple.jsx

--- Create/CreateCameraRig-Simple.jsx
+++ Create/CreateCameraRig-Simple.jsx
@@ -2,7 +2,7 @@
  * ?                  CreateCameraRig-Simple.jsx
  * @author         :  Jason Schwarz (https://hellolovely.tv)
  * @email          :  hello@hellolovely.tv
- * @version        :  1.0.0
+ * @version        :  1.0.1
  * @createdFor     :  Adobe After Effects CC 2024 (Version 24.1.0 Build 78)
  * @description    :  Create a simple camera rig.
  *========================================================================**/
@@ -11,91 +11,96 @@
 
     app.beginUndoGroup("Simple Camera Rig");
 
-    var myComp = app.project.activeItem;
-
-    if (myComp != null && (myComp instanceof CompItem)) {
-
-        var w = myComp.width / 2;
-        var h = myComp.height / 2;
-        var newCamera = myComp.layers.addCamera("Rigged Camera", [w, h]); //make a new camera
-        newCamera.position.setValue([w, h]);
-        var myLayer = myComp.selectedLayers;
-        if (myLayer) {
-            var bankNull = myComp.layers.addNull();
-            bankNull.source.name = "CamBank";
-            bankNull.threeDLayer = true;
-            bankNull.shy = true;
-            bankNull.enabled = false;
-
-            var pitchNull = myComp.layers.addNull();
-            pitchNull.source.name = "CamPitch";
-            pitchNull.threeDLayer = true;
-            pitchNull.shy = true;
-            pitchNull.enabled = false;
-
-            var headingNull = myComp.layers.addNull(myComp.duration);
-            headingNull.source.name = "CamHeading";
-            headingNull.threeDLayer = true;
-            headingNull.shy = true;
-            headingNull.enabled = false;
-
-            var controlNull = myComp.layers.addNull();
-            controlNull.source.name = "Camera Control";
-            controlNull.threeDLayer = true;
-            controlNull.shy = false;
-            myComp.hideShyLayers = true;
-            newCamera.parent = bankNull;
-            bankNull.parent = pitchNull;
-            pitchNull.parent = headingNull;
-            headingNull.parent = controlNull;
-
-            var pitch = controlNull("Effects").addProperty("Angle Control");
-            pitch.name = 'Pitch (X)';
-
-            var heading = controlNull("Effects").addProperty("Angle Control");
-            heading.name = 'Heading (Y)';
-
-            var bank = controlNull("Effects").addProperty("Angle Control");
-            bank.name = 'Bank (Z)';
-
-            var tracking = controlNull("Effects").addProperty("ADBE Slider Control");
-            tracking.name = 'Tracking Control';
-            tracking.slider.setValue(0);
-
-            var xPosition = controlNull("Effects").addProperty("ADBE Slider Control");
-            xPosition.name = '(X) Position';
-            xPosition.slider.setValue([w]);
-
-            var yPosition = controlNull("Effects").addProperty("ADBE Slider Control");
-            yPosition.name = '(Y) Position';
-            yPosition.slider.setValue([h]);
-
-            var zPosition = controlNull("Effects").addProperty("ADBE Slider Control");
-            zPosition.name = '(Z) Position';
-            zPosition.slider.setValue(0);
-
-            var camPositionExpression = 'x = effect("(X) Position")("Slider");y = effect("(Y) Position")("Slider");z = effect("(Z) Position")("Slider");[x, y,z];';
-            controlNull.position.expression = camPositionExpression;
-
-            var CamBankZ = 'value - [thisComp.layer("Camera Control").effect("Bank (Z)")("Angle")];';
-            bankNull.zRotation.expression = CamBankZ;
-
-            var CamPitchX = 'value - [thisComp.layer("Camera Control").effect("Pitch (X)")("Angle")];';
-            pitchNull.xRotation.expression = CamPitchX;
-
-            var CamHeadingY = 'value - [thisComp.layer("Camera Control").effect("Heading (Y)")("Angle")];';
-            pitchNull.yRotation.expression = CamHeadingY;
-
-            var positionExpression = 'value - [0,0,cameraOption.zoom] + [0,0,thisComp.layer("Camera Control").effect("Tracking Control")("Slider")];';
-            newCamera.position.expression = positionExpression;
-            app.endUndoGroup();
-
-        } else {
-            alert("Please select at least one layer");
-        }
-    } else {
-        alert("Please select an active comp to use this script");
+    function exit(msg) {
+        if (msg) alert(msg);
+        app.endUndoGroup();
     }
+
+    var comp = app.project.activeItem;
+    if (!comp || !(comp instanceof CompItem)) {
+        return exit("Please select an active comp to use this script.");
+    }
+
+    var w = comp.width  / 2;
+    var h = comp.height / 2;
+
+    var newCamera = comp.layers.addCamera("Rigged Camera", [w, h]);
+    newCamera.position.setValue([w, h]);
+
+    var bankNull = comp.layers.addNull();
+    bankNull.source.name  = "CamBank";
+    bankNull.threeDLayer  = true;
+    bankNull.shy          = true;
+    bankNull.enabled      = false;
+
+    var pitchNull = comp.layers.addNull();
+    pitchNull.source.name = "CamPitch";
+    pitchNull.threeDLayer = true;
+    pitchNull.shy         = true;
+    pitchNull.enabled     = false;
+
+    var headingNull = comp.layers.addNull();
+    headingNull.source.name = "CamHeading";
+    headingNull.threeDLayer = true;
+    headingNull.shy         = true;
+    headingNull.enabled     = false;
+
+    var controlNull = comp.layers.addNull();
+    controlNull.source.name = "Camera Control";
+    controlNull.threeDLayer = true;
+    controlNull.shy         = false;
+
+    comp.hideShyLayers  = true;
+    newCamera.parent    = bankNull;
+    bankNull.parent     = pitchNull;
+    pitchNull.parent    = headingNull;
+    headingNull.parent  = controlNull;
+
+    var fx = controlNull("Effects");
+
+    var pitch = fx.addProperty("Angle Control");
+    pitch.name = "Pitch (X)";
+
+    var heading = fx.addProperty("Angle Control");
+    heading.name = "Heading (Y)";
+
+    var bank = fx.addProperty("Angle Control");
+    bank.name = "Bank (Z)";
+
+    var tracking = fx.addProperty("ADBE Slider Control");
+    tracking.name = "Tracking Control";
+    tracking.slider.setValue(0);
+
+    var xPosition = fx.addProperty("ADBE Slider Control");
+    xPosition.name = "(X) Position";
+    xPosition.slider.setValue(w);
+
+    var yPosition = fx.addProperty("ADBE Slider Control");
+    yPosition.name = "(Y) Position";
+    yPosition.slider.setValue(h);
+
+    var zPosition = fx.addProperty("ADBE Slider Control");
+    zPosition.name = "(Z) Position";
+    zPosition.slider.setValue(0);
+
+    controlNull.position.expression =
+        'x = effect("(X) Position")("Slider");' +
+        'y = effect("(Y) Position")("Slider");' +
+        'z = effect("(Z) Position")("Slider");' +
+        '[x, y, z];';
+
+    bankNull.zRotation.expression =
+        'value - [thisComp.layer("Camera Control").effect("Bank (Z)")("Angle")];';
+
+    pitchNull.xRotation.expression =
+        'value - [thisComp.layer("Camera Control").effect("Pitch (X)")("Angle")];';
+
+    pitchNull.yRotation.expression =
+        'value - [thisComp.layer("Camera Control").effect("Heading (Y)")("Angle")];';
+
+    newCamera.position.expression =
+        'value - [0,0,cameraOption.zoom] + ' +
+        '[0,0,thisComp.layer("Camera Control").effect("Tracking Control")("Slider")];';
 
     app.endUndoGroup();
 
